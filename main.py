64a6aa3a745ca7ffdb25af30c69c6268117c0c28
israr <isrargul00@gmail.com>
2021-03-18 17:53:10 +0500

    done

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ import requests
 notfy = ToastNotifier()
 
 def main():
-    city = "hyderabad"
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=5d82bd007e89d3d09e87df9f526cd725"
-    city_weather = requests.get(url.format(city)).json()
-    print(city_weather['main']['temp'])
-    print('-----------start -0---------------------------')
-    notfy.show_toast('Weather',"temperature" + str(city_weather['main']['temp']), icon_path='icon/icon.ico', duration=10)
-    print('-----------end -0---------------------------')
-
-
+    notfy.show_toast('Python',"Python Deskstop Notifications System ", duration=10)
 
 if __name__ == '__main__':
     main()
